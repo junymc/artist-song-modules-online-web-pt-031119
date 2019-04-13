68cdb7680c11955dc757dd418cc110f67496dcbe
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-13 21:53:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,16 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -18,13 +23,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+  #
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
